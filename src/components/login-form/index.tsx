4ec42c92a09c88354ac544f5438294bebc5bde31
João Jeefer da Silva Félix <ter_jfelix@arezzo.com.br>
2024-09-30 23:36:21 -0300

    feat: :sparkles: add flogin function to login form component

--- src/components/login-form/index.tsx
+++ src/components/login-form/index.tsx
@@ -1,7 +1,30 @@
-/* eslint-disable @next/next/no-img-element */
-import React from 'react';
+"use client";
 
-export const LoginForm: React.FC = () => {
+/* eslint-disable @next/next/no-img-element */
+import { api } from '@/services';
+import { useRouter } from 'next/navigation';
+import React, { FormEvent } from 'react';
+
+export const LoginForm = () => {
+    const router = useRouter();
+    
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+
+        const data = {
+            username: formData.get('username'),
+            password: formData.get('password'),
+        };
+
+        const response = await api.post('/auth/login', data, { headers: { 'Content-Type': 'application/json' } });
+
+        if (response.status === 200) {
+            router.push('/dashboard');
+        }
+    }
+
     return (
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -16,18 +39,18 @@ export const LoginForm: React.FC = () => {
             </div>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form action="#" method="POST" className="space-y-6">
+            <form onSubmit={handleSubmit} method="POST" className="space-y-6">
                 <div>
-                <label htmlFor="user" className="block text-sm font-medium leading-6 text-gray-900">
+                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
                     Usuário
                 </label>
                 <div className="mt-2">
                     <input
-                    id="user"
-                    name="user"
+                    id="username"
+                    name="username"
                     type="text"
                     required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                 </div>
                 </div>
@@ -50,7 +73,7 @@ export const LoginForm: React.FC = () => {
                     type="password"
                     required
                     autoComplete="current-password"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                 </div>
                 </div>
