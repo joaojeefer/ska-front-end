bdd78715b56d7ad9fb98e9fbdc43f72963fef53d
João Jeefer da Silva Félix <ter_jfelix@arezzo.com.br>
2024-10-03 20:04:58 -0300

    feat: :sparkles: add error-content component

---
+++ src/components/error-content/types.ts
@@ -0,0 +1,4 @@
+export type ErrorContentProps = {
+    title: string;
+    description: string;
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
-export {Header} from './header';
-export {LoginForm} from './login-form';
+export { Header } from './header';
+export { LoginForm } from './login-form';
+export { ErrorContent } from './error-content';
