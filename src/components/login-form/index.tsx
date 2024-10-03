a7af4c170ce5cf87f5f1ce4c1645c84d9956244d
João Jeefer da Silva Félix <ter_jfelix@arezzo.com.br>
2024-10-03 20:10:02 -0300

    refactor: :recycle: update login-form component

--- src/components/login-form/index.tsx
+++ src/components/login-form/index.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 /* eslint-disable @next/next/no-img-element */
-import { api } from '@/services';
-import { useRouter } from 'next/navigation';
-import React, { FormEvent } from 'react';
+import React, { FormEvent, useContext } from 'react';
+import { AuthContext } from '@/contexts';
+import { SignInApiPayload } from '@/api/types';
 
 export const LoginForm = () => {
-    const router = useRouter();
+    const { handleSignIn } = useContext(AuthContext);
     
     async function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
@@ -16,23 +16,14 @@ export const LoginForm = () => {
         const data = {
             username: formData.get('username'),
             password: formData.get('password'),
-        };
+        } as SignInApiPayload;
 
-        const response = await api.post('/auth/login', data, { headers: { 'Content-Type': 'application/json' } });
-
-        if (response.status === 200) {
-            router.push('/machines');
-        }
+        await handleSignIn(data);
     }
 
     return (
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-                alt="Your Company"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                className="mx-auto h-10 w-auto"
-            />
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                 Faça seu login
             </h2>
@@ -60,11 +51,6 @@ export const LoginForm = () => {
                     <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                     Senha
                     </label>
-                    {/* <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                        Esqueceu a senha?
-                    </a>
-                    </div> */}
                 </div>
                 <div className="mt-2">
                     <input
@@ -87,13 +73,6 @@ export const LoginForm = () => {
                 </button>
                 </div>
             </form>
-
-            {/* <p className="mt-10 text-center text-sm text-gray-500">
-                Crie sua conta{' '}
-                <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                aqui
-                </a>
-            </p> */}
             </div>
         </div>
     )
