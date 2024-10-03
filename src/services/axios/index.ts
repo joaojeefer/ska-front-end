bb2ccf9ef7034e2005087b97dc05bf19cc7a3175
João Jeefer da Silva Félix <ter_jfelix@arezzo.com.br>
2024-10-03 00:09:27 -0300

    feat: :sparkles: add auth token to default api headers

--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -1,4 +1,9 @@
 import axios from "axios";
+import { parseCookies } from 'nookies';
+
+const USER_TOKEN_KEY = 'ska-app-token';
+
+const { [USER_TOKEN_KEY]: token } = parseCookies();
 
 export const api = axios.create({
     baseURL: 'http://localhost:3001',
@@ -7,3 +12,7 @@ export const api = axios.create({
         'Content-Type': 'application/json',
     },
 });
+
+if (token) {
+    api.defaults.headers['Authorization'] = `Bearer ${JSON.parse(token).token}`;
+}
